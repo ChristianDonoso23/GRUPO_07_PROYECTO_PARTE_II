bcb8eeca3552652684a5c5027c44b987c3e1cd2b
Christian Donoso <cedonoso@espe.edu.ec>
2025-11-11 09:49:29 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication02_Con Autenticacion/Controllers/ConsultaController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication02_Con_Autenticacion.Models;
+
+namespace WebApplication02_Con_Autenticacion.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        private ProyectoVeris_Context db = new ProyectoVeris_Context();
+
+        // GET: Consulta
+        [Authorize(Roles = "SuperAdmin, Administrador, Medico")]
+        public ActionResult Index()
+        {
+            var consultas = db.consultas.Include(c => c.pacientes).Include(c => c.medicos);
+            return View(consultas.ToList());
+        }
+
+        // GET: Consulta/Details/5
+        [Authorize(Roles = "SuperAdmin, Administrador, Medico")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            consultas consulta = db.consultas.Find(id);
+            if (consulta == null)
+                return HttpNotFound();
+
+            return View(consulta);
+        }
+
+        // GET: Consulta/Create
+        [Authorize(Roles = "SuperAdmin, Medico")]
+        public ActionResult Create()
+        {
+            ViewBag.IdPaciente = new SelectList(db.pacientes, "IdPaciente", "Nombre");
+            ViewBag.IdMedico = new SelectList(db.medicos, "IdMedico", "Nombre");
+            return View();
+        }
+
+        // POST: Consulta/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin, Medico")]
+        public ActionResult Create([Bind(Include = "IdConsulta,IdMedico,IdPaciente,FechaConsulta,HI,HF,Diagnostico")] consultas consulta)
+        {
+            ViewBag.IdPaciente = new SelectList(db.pacientes, "IdPaciente", "Nombre", consulta.IdPaciente);
+            ViewBag.IdMedico = new SelectList(db.medicos, "IdMedico", "Nombre", consulta.IdMedico);
+
+            if (!consulta.HorarioValido())
+            {
+                ModelState.AddModelError("HF", "La hora de fin debe ser mayor que la hora de inicio.");
+            }
+
+            if (!consulta.FechaValida())
+            {
+                ModelState.AddModelError("FechaConsulta", "La fecha de la consulta no puede ser futura.");
+            }
+
+            if (!ModelState.IsValid)
+                return View(consulta);
+
+            db.consultas.Add(consulta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        // GET: Consulta/Edit/5
+        [Authorize(Roles = "SuperAdmin, Medico")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            consultas consulta = db.consultas.Find(id);
+            if (consulta == null)
+                return HttpNotFound();
+
+            ViewBag.IdPaciente = new SelectList(db.pacientes, "IdPaciente", "Nombre", consulta.IdPaciente);
+            ViewBag.IdMedico = new SelectList(db.medicos, "IdMedico", "Nombre", consulta.IdMedico);
+            return View(consulta);
+        }
+
+        // POST: Consulta/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin, Medico")]
+        public ActionResult Edit([Bind(Include = "IdConsulta,IdMedico,IdPaciente,FechaConsulta,HI,HF,Diagnostico")] consultas consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consulta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdPaciente = new SelectList(db.pacientes, "IdPaciente", "Nombre", consulta.IdPaciente);
+            ViewBag.IdMedico = new SelectList(db.medicos, "IdMedico", "Nombre", consulta.IdMedico);
+            return View(consulta);
+        }
+
+        // GET: Consulta/Delete/5
+        [Authorize(Roles = "SuperAdmin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            consultas consulta = db.consultas.Find(id);
+            if (consulta == null)
+                return HttpNotFound();
+
+            return View(consulta);
+        }
+
+        // POST: Consulta/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            consultas consulta = db.consultas.Find(id);
+            db.consultas.Remove(consulta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                db.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication02_Con Autenticacion/Controllers/RecetaController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication02_Con_Autenticacion.Models;
+
+namespace WebApplication02_Con_Autenticacion.Controllers
+{
+    public class RecetaController : Controller
+    {
+        private ProyectoVeris_Context db = new ProyectoVeris_Context();
+
+        // GET: Receta
+        [Authorize(Roles = "SuperAdmin, Administrador, Medico")]
+        public ActionResult Index()
+        {
+            var recetas = db.recetas.Include(r => r.consultas).Include(r => r.medicamentos);
+            return View(recetas.ToList());
+        }
+
+        // GET: Receta/Details/5
+        [Authorize(Roles = "SuperAdmin, Administrador, Medico")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            recetas receta = db.recetas.Find(id);
+            if (receta == null)
+                return HttpNotFound();
+
+            return View(receta);
+        }
+
+        // GET: Receta/Create
+        [Authorize(Roles = "SuperAdmin, Medico")]
+        public ActionResult Create()
+        {
+            ViewBag.IdConsulta = new SelectList(db.consultas, "IdConsulta", "Diagnostico");
+            ViewBag.IdMedicamento = new SelectList(db.medicamentos, "IdMedicamento", "Nombre");
+            return View();
+        }
+
+        // POST: Receta/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin, Medico")]
+        public ActionResult Create([Bind(Include = "IdReceta,IdConsulta,IdMedicamento,Cantidad")] recetas receta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.recetas.Add(receta);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdConsulta = new SelectList(db.consultas, "IdConsulta", "Diagnostico", receta.IdConsulta);
+            ViewBag.IdMedicamento = new SelectList(db.medicamentos, "IdMedicamento", "Nombre", receta.IdMedicamento);
+            return View(receta);
+        }
+
+        // GET: Receta/Edit/5
+        [Authorize(Roles = "SuperAdmin, Medico")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            recetas receta = db.recetas.Find(id);
+            if (receta == null)
+                return HttpNotFound();
+
+            ViewBag.IdConsulta = new SelectList(db.consultas, "IdConsulta", "Diagnostico", receta.IdConsulta);
+            ViewBag.IdMedicamento = new SelectList(db.medicamentos, "IdMedicamento", "Nombre", receta.IdMedicamento);
+            return View(receta);
+        }
+
+        // POST: Receta/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin, Medico")]
+        public ActionResult Edit([Bind(Include = "IdReceta,IdConsulta,IdMedicamento,Cantidad")] recetas receta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(receta).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdConsulta = new SelectList(db.consultas, "IdConsulta", "Diagnostico", receta.IdConsulta);
+            ViewBag.IdMedicamento = new SelectList(db.medicamentos, "IdMedicamento", "Nombre", receta.IdMedicamento);
+            return View(receta);
+        }
+
+        // GET: Receta/Delete/5
+        [Authorize(Roles = "SuperAdmin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            recetas receta = db.recetas.Find(id);
+            if (receta == null)
+                return HttpNotFound();
+
+            return View(receta);
+        }
+
+        // POST: Receta/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "SuperAdmin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            recetas receta = db.recetas.Find(id);
+            db.recetas.Remove(receta);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                db.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
